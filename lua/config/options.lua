e835cee80f17e8a5d2ae16aa35ce1141b9c90e51
thdtt <thanhdat.vnu@gmail.com>
2024-12-01 15:55:00 +0700

    #nvim: lazyvim + nightfox theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,10 +3,38 @@
 -- Add any additional options here
 --
 --
-local opt = vim.opt
 
-opt.number = true
-opt.relativenumber = false
+vim.g.mapleader = " "
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 0
+vim.opt.expandtab = true
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
 
 -- setup for python
 -- LSP Server to use for Python.
@@ -18,3 +46,5 @@ vim.g.lazyvim_python_ruff = "ruff"
 -- set color scheme
 vim.g.nightflyTransparent = true
 vim.g.nightflyWinSeparator = 2
+
+-- Add asterisks in block comments
