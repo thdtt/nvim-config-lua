d01d2080d9327f09ab06519d9af992a690861fe4
thdtt <thanhdat.vnu@gmail.com>
2024-12-01 21:46:27 +0700

    #nvim: remap for split horizontal and vertical

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -30,7 +30,6 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
 vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.splitkeep = "cursor"
