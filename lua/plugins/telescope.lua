a6210d02850bf65107f225672ef2345dc76745cf
thdtt <thanhdat.vnu@gmail.com>
2024-12-31 09:50:43 +0700

    #nvim: add file name before qualified path in telescope search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@
 return {
   "nvim-telescope/telescope.nvim",
   opts = function(_, opts)
+    -- Ensure defaults are defined
     local defaults = opts.defaults or {}
+
+    -- Customize vimgrep_arguments
     defaults.vimgrep_arguments = {
       "rg",
       "--color=never",
@@ -14,6 +17,14 @@ return {
       "--ignore-file",
       "~/.ignore", -- Use your custom ignore file
     }
+
+    -- Add the custom path_display function
+    defaults.path_display = function(opts, path)
+      local tail = require("telescope.utils").path_tail(path)
+      return string.format("%s (%s)", tail, path), { { { 0, #tail }, "Constant" } }
+    end
+
+    -- Assign the modified defaults back to opts
     opts.defaults = defaults
   end,
 }
