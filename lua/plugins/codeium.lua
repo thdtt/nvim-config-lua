adda658098fa40d30536a86ede33bb8ec1f8266f
thdtt <thanhdat.vnu@gmail.com>
2025-01-04 17:58:20 +0700

    #nvim: tab for auto completion

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+        enable_cmp_source = true,
+        virtual_text = {
+          enabled = false,
+        },
+      })
+    end,
+  },
+}
